15f08b710d49fec2145753a98c955fb84052496f
eSSeGH <ste.virgadaula@gmail.com>
2023-02-14 16:34:53 +0100

    JS milestone 1 accomplished

--- js/app.js
+++ js/app.js
@@ -3,49 +3,57 @@ const team = [
         firstName: "Wayne", 
         lastName: "Barnett",
         role: "Founder & CEO",
-        getImageSrc: getImageSrc,
+        getImgSrc: getImgSrc,
     },
     {
         firstName: "Angela", 
         lastName: "Caroll",
         role: "Chief Editor",
-        getImageSrc: getImageSrc,
+        getImgSrc: getImgSrc,
     },
     {
         firstName: "Walter", 
         lastName: "Gordon",
         role: "Office Manager",
-        getImageSrc: getImageSrc,
+        getImgSrc: getImgSrc,
     },
     {
         firstName: "Angela", 
         lastName: "Lopez",
         role: "Social Media Manager",
-        getImageSrc: getImageSrc,
+        getImgSrc: getImgSrc,
     },
     {
         firstName: "Scott", 
         lastName: "Estrada",
         role: "Developer",
-        getImageSrc: getImageSrc,
+        getImgSrc: getImgSrc,
     },
     {
         firstName: "Barbara", 
         lastName: "Ramos",
         role: "Graphic Designer",
-        getImageSrc: getImageSrc,
+        getImgSrc: getImgSrc,
     }
 ]
 
 console.log(team)
 
-const imgSrcWayne = team[0].getimgSrc()
-console.log(imgSrcWayne)
+for (i=0; i<team.length; i++) {
+
+    for (let key in team[i]) {
+        console.log(team[i][key])
+    }
+    let imgSrc = team[i].getImgSrc()
+    console.log(imgSrc)
+}
+
+
 
 // FUNCTIONS 
 
 // GET img SRC FUNC
-function getimgSrc() {
+function getImgSrc() {
     const firstName = this.firstName
     const lastName = this.lastName
     const role = this.role
